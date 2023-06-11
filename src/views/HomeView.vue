e4a8668536b1ef673b5586a39a65ac6940bb7639
Pedro Henrique <pedrohenri1606@gmail.com>
2023-06-11 14:37:25 -0300

    Aprendizado sobre conexão com APIs utilizando Axios

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,31 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <div v-for="(pokemon, i) in pokemons" :key="i"> 
+      <p>{{ pokemon.name }}</p>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, onMounted, ref } from 'vue';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+
+
+import api from "@/client/pokemon";
 
 export default defineComponent({
   name: 'HomeView',
   components: {
     HelloWorld,
   },
+  setup(){
+    const pokemons = ref([])
+
+    const fetchPokemons = () => api.get("/pokemon?limit=20").then((response) => pokemons.value = response.data.results)
+
+    onMounted(fetchPokemons);
+
+    return {pokemons}
+  }
 });
 </script>
